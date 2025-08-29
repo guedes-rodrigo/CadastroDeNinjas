3b376d28dd9f49f5e82df5065ff8ada9dd9d6939
Rodrigo Guedes <guedes_33@hotmail.com>
2025-08-28 22:25:29 -0300

    add entity and add jpa in pom

--- src/main/java/dev/guedes/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/guedes/CadastroDeNinjas/NinjaController.java
@@ -1,13 +1,12 @@
 package dev.guedes.CadastroDeNinjas;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é a minha primeira mensagem nesta rota!";
