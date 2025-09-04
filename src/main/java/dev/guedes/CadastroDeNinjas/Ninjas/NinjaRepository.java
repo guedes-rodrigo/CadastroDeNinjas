294507df6f7544ddfac17c365fdf9efb8370182c
Rodrigo Guedes <guedes_33@hotmail.com>
2025-09-04 11:50:24 -0300

    feat: Criado Ninja repository

---
+++ src/main/java/dev/guedes/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,7 @@
+package dev.guedes.CadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
+}
+
